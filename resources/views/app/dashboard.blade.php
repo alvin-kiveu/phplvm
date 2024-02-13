9d95fcd0a34394c5ce2ddae90437b354b8b4c98b
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2024-02-13 16:50:20 +0300

    adding new data

--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -2,7 +2,53 @@
 @section('content')
     <div class="row">
 
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Ubuntu Information</h5>
+                </div>
+                <div class="card-body">
+                    <!-- Add your Ubuntu information content here -->
+                    <p>Version: {{ php_uname('v') }}</p>
+                    <p>Kernel version: {{ php_uname('r') }}</p>
+                    <p>Machine type: {{ php_uname('m') }}</p>
+                    <p>Operating system: {{ php_uname('s') }}</p>
+                    <p>Hostname: {{ php_uname('n') }}</p>
+                    <p>Release name: {{ php_uname('release') }}</p>
+                    <p>Processor type: {{ php_uname('p') }}</p>
+                    <p>Node name: {{ php_uname('n') }}</p>
+                    <p>Processor identifier: {{ php_uname('i') }}</p>
+                    <p>Architecture: {{ php_uname('m') }}</p>
+                    <!-- Add more information as needed -->
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h5>OS Rubin Information</h5>
+                </div>
+                <div class="card-body">
+                    <!-- Add your OS Rubin information content here -->
+                    <p>Version: {{ $os_rubin_version }}</p>
+                    <p>Kernel version: {{ $os_rubin_kernel }}</p>
+                    <p>Machine type: {{ $os_rubin_machine_type }}</p>
+                    <p>Operating system: {{ $os_rubin_os }}</p>
+                    <p>Hostname: {{ $os_rubin_hostname }}</p>
+                    <p>Release name: {{ $os_rubin_release }}</p>
+                    <p>Processor type: {{ $os_rubin_processor_type }}</p>
+                    <p>Node name: {{ $os_rubin_node_name }}</p>
+                    <p>Processor identifier: {{ $os_rubin_processor_id }}</p>
+                    <p>Architecture: {{ $os_rubin_architecture }}</p>
+                    <!-- Add more information as needed -->
+                </div>
+            </div>
+        </div>
+
         <div class="col-xl-12 col-md-12">
+
+
 
             <div class="col-xl-12 col-md-12">
                 <div class="card table-card">
@@ -43,7 +89,7 @@
                                         echo "<td>{$client->name}</td>";
                                         echo "<td>{$client->username}</td>";
                                         echo "<td>{$client->password}</td>";
-                                        echo "<td>{$client->ip_address}</td>";
+                                        echo "<td>{$client->ipaddress}</td>";
                                         echo "<td>{$client->type}</td>";
                                         echo "</tr>";
                                         $counter++; // Increment the counter
